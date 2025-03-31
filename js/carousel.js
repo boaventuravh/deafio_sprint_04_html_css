40c43456cac6960cdc654f3a3d245ca58afa17f4
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-03-31 10:52:57 -0300

    alterar lógica carrossel c inclusão de funções back e next

--- js/carousel.js
+++ js/carousel.js
@@ -3,16 +3,39 @@ let indiceImagem = 0;
 alternarImagens();
 
 function alternarImagens(){
+    next();
+
+    setTimeout(alternarImagens, 3500);
+}
+
+function next(){
+    mudarIndice(true);
+    exibirImagemIndiceAtual();
+}
+
+function back(){
+    mudarIndice(false);
+    exibirImagemIndiceAtual();
+}
+
+function mudarIndice(aumentar){
+    aumentar ? indiceImagem++ : indiceImagem--;
+    
+    if(indiceImagem > imagens.length - 1){
+        indiceImagem = 0;
+    }
+
+    if(indiceImagem < 0){
+        indiceImagem = imagens.length - 1;
+    }
+
+}
+
+function exibirImagemIndiceAtual(){
     imagens.forEach(imagem => {
         imagem.style.display = 'none';
     });
 
     imagens[indiceImagem].style.display = 'block';
-
-    indiceImagem++;
-    if(indiceImagem > imagens.length - 1){
-        indiceImagem = 0;
-    }       
-
-    setTimeout(alternarImagens, 3500);
 }
+
