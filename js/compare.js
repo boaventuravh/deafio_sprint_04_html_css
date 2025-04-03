1b241035779f50379bf4ab322d685f30e2bbf9b0
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-04-03 16:54:15 -0300

    refatorar nomes + add carros no array

--- js/compare.js
+++ js/compare.js
@@ -1,10 +1,27 @@
+/*
+
+ 1 - XL Cabine Simples 2.2 4x4 MT 2022 - R$ 183.850,00
+ 2 - XLS 2.2 Diesel 4X4 AT 2022 - R$ 220.690,00
+ 3 - Storm 3.2 Diesel 4X4 AT 2022 - R$ 222.790,00
+
+ */
+
 let carArr = [];
+
+carArr.push(new Car("../img/XL Cabine.jpg", "XL Cabine Simples 2.2 4x4 MT 2022",
+     511, 1821, 232, 1234, 2.2, 160, 1420, "Aço Estampado 16", 183850.00));
+
+carArr.push(new Car("../img/xls 2.2 diesel.jpg", "XLS 2.2 Diesel 4X4 AT 2022",
+     511, 1821, 232, 1076, 2.2, 160, 1180, "Aço Estampado 16", 220690.00));
+
+carArr.push(new Car("../img/storm.jpg", "Storm 3.2 Diesel 4X4 AT 2022", 
+    511, 1821, 232, 1040, 3.2, 200, 1180, "Liga Leve 17", 222790.00));
 
 class Car {   
 
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-        this.nome = nome;
-        this.preco = preco;
+    constructor(image, modelo, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, preco){
+        this.image = image;
+        this.modelo = modelo;        
         this.alturaCacamba = alturaCacamba;
         this.alturaVeiculo = alturaVeiculo;
         this.alturaSolo = alturaSolo;
@@ -13,14 +30,14 @@ class Car {
         this.potencia = potencia;
         this.volumeCacamba = volumeCacamba;
         this.roda = roda;
-        this.image = image;
+        this.preco = preco;
     }
 } 
 
 // search on array if exist carClass returning 1 if not return -1
 function GetCarArrPosition(arr, carClass) {
     for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
+        if(arr[i].modelo  === carClass.modelo)
             return i;
     }
     return -1;
