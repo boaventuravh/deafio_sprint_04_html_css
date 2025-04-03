89774e996d2baf7bbd33ca5f9f851b82f0a3b2e4
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-04-03 11:28:22 -0300

    iniciar implementação compare

---
+++ js/compare.js
@@ -0,0 +1,59 @@
+let carArr = [];
+
+class Car {   
+
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
+        this.nome =nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        
+    }
+} 
+
+// search on array if exist carClass returning 1 if not return -1
+function GetCarArrPosition(arr, carClass) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].nome  === carClass.nome)
+            return i;
+    }
+    return -1;
+}
+
+function SetCarToCompare(checkbox, carClass) {
+   
+    if(carClass instanceof Car){       
+        if(checkbox.checked){
+                
+            
+        } else {
+          
+        } 
+    } else {
+        throw "You need set a Car Class";
+    }
+}
+
+function ShowCompare() {
+    if(carArr.length < 2) {
+        alert("Precisa marcar 2 carros para apresentar a comparação");
+        return;
+    }
+
+    UpdateCompareTable();
+    document.getElementById("compare").style.display = "block";
+}
+
+function HideCompare(){
+    document.getElementById("compare").style.display = "none"; 
+}
+
+function UpdateCompareTable() {
+    
+}
