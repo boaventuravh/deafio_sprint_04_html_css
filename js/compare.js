592122fc242fdf6f2d6dcac9cf5cf31bfc2b1396
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-04-03 11:42:55 -0300

    alterar construtor

--- js/compare.js
+++ js/compare.js
@@ -3,7 +3,7 @@ let carArr = [];
 class Car {   
 
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-        this.nome =nome;
+        this.nome = nome;
         this.preco = preco;
         this.alturaCacamba = alturaCacamba;
         this.alturaVeiculo = alturaVeiculo;
@@ -13,7 +13,7 @@ class Car {
         this.potencia = potencia;
         this.volumeCacamba = volumeCacamba;
         this.roda = roda;
-        
+        this.image = image;
     }
 } 
 
